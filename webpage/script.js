80d9237117a3d44476f8a54e9cff26ef2a35905e
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-09-13 21:04:53 +0100

    make the svg an image element

--- webpage/script.js
+++ webpage/script.js
@@ -4,7 +4,11 @@ init().then();
 function process_conversion() {
 	var text = document.getElementById("input_text").value;
 	var result = to_gallifreyan(text);
-	console.log("result: "+result);
+	console.log("result: "+result);	
+	if (result.startsWith("<svg")) {
+		result = window.btoa(result);
+		result = `<img src="data:image/svg+xml;base64,${result}"/>`
+	}
 	var container = document.getElementById("svg_container");
 	container.innerHTML = result;
 }
