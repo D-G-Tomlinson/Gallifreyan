7e48b0792df81803f69a08bf24da7ee7de2ddb3c
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-09-16 02:30:59 +0100

    add download, but must specify stroke

--- webpage/script.js
+++ webpage/script.js
@@ -1,4 +1,8 @@
 import init, {to_gallifreyan } from "./pkg/gallifreyan.js";
 init().then();
-function process_conversion() {	document.getElementById("svg_box").innerHTML = to_gallifreyan(document.getElementById("input_text").value);}
+function process_conversion() {
+	var result = to_gallifreyan(document.getElementById("input_text").value);
+	document.getElementById("svg_box").innerHTML = result;
+	document.getElementById("download_link").setAttribute("href","data:image/svg+xml;base64," + btoa(result));
+}
 document.getElementById("input_button").addEventListener("click",process_conversion,false);
