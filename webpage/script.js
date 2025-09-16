26de29a59cc170f63c8b6e7766efe24d7a37280d
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-09-16 03:02:59 +0100

    can download, in proper colour

--- webpage/script.js
+++ webpage/script.js
@@ -1,8 +1,18 @@
 import init, {to_gallifreyan } from "./pkg/gallifreyan.js";
 init().then();
+function add_download(text) {
+	var templ = document.createElement("template");
+	templ.innerHTML = text.trim();
+	let svg = templ.content.firstChild;
+
+	let style = window.getComputedStyle(document.getElementById("generated_svg"),null);
+	svg.setAttribute("stroke",style.stroke);
+	svg.setAttribute("fill", style.fill);
+	document.getElementById("download_link").setAttribute("href","data:image/svg+xml;base64," + btoa(svg.outerHTML));
+}
 function process_conversion() {
-	var result = to_gallifreyan(document.getElementById("input_text").value);
+	let result = to_gallifreyan(document.getElementById("input_text").value);
 	document.getElementById("svg_box").innerHTML = result;
-	document.getElementById("download_link").setAttribute("href","data:image/svg+xml;base64," + btoa(result));
+	add_download(result);
 }
 document.getElementById("input_button").addEventListener("click",process_conversion,false);
