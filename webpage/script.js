40d491ff5cc295521cdf7a2bc26e700d68b0d21f
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-09-16 23:49:44 +0100

    new download button

--- webpage/script.js
+++ webpage/script.js
@@ -1,10 +1,30 @@
 import init, {to_gallifreyan } from "./pkg/gallifreyan.js";
 init().then();
-function add_download(text) {
-	var templ = document.createElement("template");
-	templ.innerHTML = text.trim();
-	let svg = templ.content.firstChild;
 
+const downloadhtml = '<a id="download_link" download="gallifreyan.svg"><button class="gall_button"><img id="download_img" class="pulsing" src="webpage/Images/download.svg" alt="convert"></button></a>';
+
+function get_el(html) {
+	var templ = document.createElement("template");
+	templ.innerHTML = html.trim();
+	return templ.content.firstChild;
+}
+
+function add_download(text) {
+	let svg = get_el(text);
+
+	let down_butt = document.getElementById("download_link");
+	let not_empty = document.getElementById("all_gall");
+
+	if (!not_empty) {
+		if (down_butt) {
+			down_butt.remove();
+		}
+		return;
+	}
+	if (!down_butt) {
+		let new_download = get_el(downloadhtml);
+		document.body.appendChild(new_download);
+	}
 	let style = window.getComputedStyle(document.getElementById("generated_svg"),null);
 	svg.setAttribute("stroke",style.stroke);
 	svg.setAttribute("fill", style.fill);
